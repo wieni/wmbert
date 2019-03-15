27e8a8c0ac7b604c84baee12beb74c9ba74660d3
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-03-15 13:54:35 +0100

    Added option to the selection handler to filter entities on language

--- src/Plugin/EntityReferenceSelection/WmBertSelection.php
+++ src/Plugin/EntityReferenceSelection/WmBertSelection.php
@@ -2,8 +2,13 @@
 
 namespace Drupal\wmbert\Plugin\EntityReferenceSelection;
 
+use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\Plugin\EntityReferenceSelection\DefaultSelection;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @EntityReferenceSelection(
@@ -16,10 +21,27 @@ use Drupal\Core\Entity\Plugin\EntityReferenceSelection\DefaultSelection;
  */
 class WmBertSelection extends DefaultSelection
 {
+    /** @var LanguageManagerInterface */
+    protected $languageManager;
+
+    public function __construct(
+        array $configuration,
+        $pluginId,
+        $pluginDefinition,
+        EntityManagerInterface $entityManager,
+        ModuleHandlerInterface $moduleHandler,
+        AccountInterface $currentUser,
+        LanguageManagerInterface $languageManager
+    ) {
+        parent::__construct($configuration, $pluginId, $pluginDefinition, $entityManager, $moduleHandler, $currentUser);
+        $this->languageManager = $languageManager;
+    }
+
     public function defaultConfiguration()
     {
         return [
             'ignored_entities' => [],
+            'same_language_only' => false,
         ] + parent::defaultConfiguration();
     }
 
@@ -28,12 +50,39 @@ class WmBertSelection extends DefaultSelection
         $query = parent::buildEntityQuery($match, $match_operator);
         $configuration = $this->getConfiguration();
         $entityType = $this->entityManager->getDefinition($configuration['target_type']);
-        $ignored = $configuration['ignored_entities'];
 
+        $ignored = $configuration['ignored_entities'];
         if ($entityType instanceof EntityTypeInterface && !empty($ignored)) {
             $query->condition($entityType->getKey('id'), $ignored, 'NOT IN');
         }
 
+        if ($configuration['same_language_only']) {
+            $query->condition(
+                $entityType->getKey('langcode'),
+                $this->languageManager->getCurrentLanguage()->getId()
+            );
+        }
+
         return $query;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(
+        ContainerInterface $container,
+        array $configuration,
+        $pluginId,
+        $pluginDefinition
+    ) {
+        return new static(
+            $configuration,
+            $pluginId,
+            $pluginDefinition,
+            $container->get('entity.manager'),
+            $container->get('module_handler'),
+            $container->get('current_user'),
+            $container->get('language_manager')
+        );
     }
 }
