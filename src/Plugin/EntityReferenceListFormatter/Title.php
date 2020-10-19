fc5cc4225a796679afb15cb66422f35abafacacc
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-10-19 20:32:48 +0200

    Add header to 'Entity title' list formatter

--- src/Plugin/EntityReferenceListFormatter/Title.php
+++ src/Plugin/EntityReferenceListFormatter/Title.php
@@ -3,6 +3,7 @@
 namespace Drupal\wmbert\Plugin\EntityReferenceListFormatter;
 
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\wmbert\EntityReferenceListFormatterPluginBase;
 
 /**
@@ -13,12 +14,21 @@ use Drupal\wmbert\EntityReferenceListFormatterPluginBase;
  */
 class Title extends EntityReferenceListFormatterPluginBase
 {
+    use StringTranslationTrait;
+
     public function getCells(EntityInterface $entity): array
     {
         $entity = $this->entityRepository->getTranslationFromContext($entity);
 
         return [
             ['#markup' => $entity->label()],
+        ];
+    }
+
+    public function getHeader(): array
+    {
+        return [
+            $this->t('Title'),
         ];
     }
 }
