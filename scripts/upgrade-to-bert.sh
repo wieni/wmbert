06f6466604cad028b4ee071ccd8e328bc16115fb
Pham Van den Eynden <32063989+akasake@users.noreply.github.com>
2023-07-12 09:46:02 +0200

    Add other notable changes in upgrade script

--- scripts/upgrade-to-bert.sh
+++ scripts/upgrade-to-bert.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 find "$@" -type f -print0 | xargs -0 gsed -i -r \
-  -e 's/wmbert/bert/g'
+  -e 's/wmbert/bert/g' \
+  -e 's/WmBertSelection/BertSelection/g' \
+  -e 's/bert\\Plugin\\EntityReferenceListFormatter/bert\\Plugin\\bert\\EntityReferenceListFormatter/g' \
+  -e 's/bert\\Plugin\\EntityReferenceLabelFormatter/bert\\Plugin\\bert\\EntityReferenceLabelFormatter/g'
