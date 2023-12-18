f81eefb03b78499b247b8af8a0601e847606b319
Robin Houtevelts <robin@houtevelts.com>
2023-12-18 12:57:34 +0700

    Update Changelog and Readme
    Add link to maintained drupal/bert

--- scripts/upgrade-to-bert.sh
+++ scripts/upgrade-to-bert.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Note: On linux you might want to use `sed` instead of `gsed`
+# on Mac you might need to install `gsed`. See https://formulae.brew.sh/formula/gnu-sed
+
 find "$@" -type f -print0 | xargs -0 gsed -i -r \
   -e 's/wmbert/bert/g' \
   -e 's/WmBertSelection/BertSelection/g' \
